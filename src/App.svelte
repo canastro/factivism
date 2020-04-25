f15e978f1131d4559486815c7bb6073b0746aab2
canastro <ricardocanastro@gmail.com>
2020-04-25 23:33:47 +0100

    fix: change page title

--- src/App.svelte
+++ src/App.svelte
@@ -4,32 +4,33 @@
 
 <style>
   header {
-	  display: flex;
-	  flex-direction: column;
-	  justify-content: center;
-	  align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
   }
+
   main {
     text-align: center;
     padding: 1em;
-	max-width: 640px;
-	width: 100%;
-	margin: 0 auto;
-	flex: 1 0 auto;
+    max-width: 640px;
+    width: 100%;
+    margin: 0 auto;
+    flex: 1 0 auto;
   }
 
   h1 {
-	text-transform: uppercase;
-	margin-block-end: 0.5rem;
+    text-transform: uppercase;
+    margin-block-end: 0.5rem;
   }
 
   footer {
-	  text-align: center;
-	  width: 100%;
-	  bottom: 0;
-	  background-color: #000;
-	  color: #fff;
-	  padding: 1rem;
+    text-align: center;
+    width: 100%;
+    bottom: 0;
+    background-color: #000;
+    color: #fff;
+    padding: 1rem;
   }
 </style>
 
@@ -49,5 +50,9 @@
 </main>
 
 <footer>
-  <p>All data is from <a href="https://ourworldindata.org/">ourworldindata</a>, you should check it out.</p>
+  <p>
+    All data is from
+    <a href="https://ourworldindata.org/">ourworldindata</a>
+    , you should check it out.
+  </p>
 </footer>
