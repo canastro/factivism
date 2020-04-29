54a6320d1aa75005c9f0e64d3243c0563d1a1c05
canastro <ricardocanastro@gmail.com>
2020-04-29 19:18:46 +0100

    feat: add loader to index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,28 @@
 <script>
-	import { goto } from '@sapper/app';
-	goto('fact');
+  import { goto } from "@sapper/app";
+  import Loader from "../components/ui/Loader.svelte";
+  goto("fact");
 </script>
 
+<style>
+  .root {
+    min-height: 700px;
+    margin: 1rem;
+  }
+</style>
+
 <svelte:head>
-	<title>Factivism</title>
+  <title>Factivism</title>
 </svelte:head>
+
+<div class="root">
+  <p>
+    Escape the
+    <a href="https://en.wikipedia.org/wiki/Availability_heuristic">
+      Availability Bias
+    </a>
+    through charts about human progress.
+  </p>
+
+  <Loader />
+</div>
