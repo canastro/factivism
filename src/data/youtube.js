723ec54d3c7c84fec7b0ace00d5c6f78a4fec03e
canastro <ricardocanastro@gmail.com>
2020-05-08 21:58:40 +0100

    feat: add thumbnail to twitter data

--- src/data/youtube.js
+++ src/data/youtube.js
@@ -4,29 +4,34 @@ export default [
     title: 'A Selfish Argument for Making the World a Better Place – Egoistic Altruism',
     url: 'https://www.youtube.com/embed/rvskMHn0sqQ',
     slug: 'egoistic-altruism',
+    thumbnail: 'https://i.ytimg.com/vi/rvskMHn0sqQ/maxresdefault.jpg'
   },
   {
     type: 'youtube',
     title: 'Bill Gates & Steven Pinker Discuss Enlightenment Now',
     url: 'https://www.youtube.com/embed/ABVVDSgihYs',
     slug: 'bill-gates-steven-pinker-enlightment-now',
+    thumbnail: 'https://i.ytimg.com/vi/ABVVDSgihYs/maxresdefault.jpg'
   },
   {
     type: 'youtube',
     title: 'Dr. Max Roser: Our World in Data',
     url: 'https://www.youtube.com/embed/HQSQ16dntbc',
     slug: 'dr-max-rose-our-world-in-data',
+    thumbnail: 'https://i.ytimg.com/vi/HQSQ16dntbc/maxresdefault.jpg'
   },
   {
     type: 'youtube',
     title: 'Is the world getting better or worse? A look at the numbers | Steven Pinker',
     url: 'https://www.youtube.com/embed/yCm9Ng0bbEQ',
     slug: 'is-the-world-getting-better-or-worse',
+    thumbnail: 'https://i.ytimg.com/vi/yCm9Ng0bbEQ/maxresdefault.jpg'
   },
   {
     type: 'youtube',
     title: 'Perspectives on Human Progress | Guillermina Sutter Schneider',
     url: 'https://www.youtube.com/embed/fY0shtNXU5A',
     slug: 'perspectives-on-human-progress',
+    thumbnail: 'https://i.ytimg.com/vi/fY0shtNXU5A/maxresdefault.jpg'
   },
 ];
