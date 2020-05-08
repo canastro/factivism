acb99566a56be8dc8f7ddffc465442e9bdb1e6c0
canastro <ricardocanastro@gmail.com>
2020-05-08 21:55:10 +0100

    fix: twitter:player width and height

--- src/routes/fact/youtube.svelte
+++ src/routes/fact/youtube.svelte
@@ -15,6 +15,8 @@
   <meta property="og:video:type" content="text/html" />
   <meta name="twitter:card" content="player">
   <meta name="twitter:player" content={fact.url} />
+  <meta name="twitter:player:width" content="1280">
+  <meta name="twitter:player:height" content="720">
 </svelte:head>
 
 <iframe title={fact.title} src={fact.url} />
